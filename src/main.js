04c2dacb70e650c113ff58b5f785cc9ada7459b4
Angele <anjelkayazade@gmail.com>
2022-10-09 15:53:29 -0400

    Finished writing the test in main.test.js and completed the function in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-function averageScore(students){
-return 1;
-}
+function averageScore(students) {
+    let totalScore = 0;
+      for (let i = 0; i < students.length; i++) {
+        totalScore += students[i].score;
+      }
+      return Number((totalScore / students.length).toFixed(1));
+    }
 
 module.exports = averageScore;
